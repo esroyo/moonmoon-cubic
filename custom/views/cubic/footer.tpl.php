0b3b95acfc725aac15ca3073e3936225025a84ff
Carles Escrig Royo <esroyo@gmail.com>
2015-02-14 20:54:41 +0100

    Minor changes on icons

--- custom/views/cubic/footer.tpl.php
+++ custom/views/cubic/footer.tpl.php
@@ -1,9 +1,8 @@
         <footer id="footer">
-            <p><?php echo _g('Powered by') . '&nbsp;<a href="http://www.moonmoon.org" target="_blank">moonmoon</a>&nbsp;<i class="icon-moon"></i>' ?>
-            <ul class="button-group tiny alert">
-                <li><a href="?type=rss10" title="<?php echo _g('Feed (RSS)') ?>"><i class="icon-rss"></i> RSS</a></li>
+            <ul class="inline-list">
+                <li><?php echo _g('Powered by') . '&nbsp;<a href="http://www.moonmoon.org" target="_blank">moonmoon</a>' ?></li>
+                <li><a class="button tiny rss" href="?type=rss10" title="<?php echo _g('Feed (RSS)') ?>"><i class="icon-rss"></i> RSS</a></li>
 
-                <li><a href="?type=atom10" title="<?php echo _g('Feed (ATOM)') ?>"><i class="icon-rss"></i> ATOM</a></li>
+                <li><a class="button tiny rss" href="?type=atom10" title="<?php echo _g('Feed (ATOM)') ?>"><i class="icon-rss"></i> ATOM</a></li>
             </ul>
-            </p>
         </footer>
