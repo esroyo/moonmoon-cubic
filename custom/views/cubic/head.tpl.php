2bf8549ed7cf872468325f2772f48693a59927b3
Carles Escrig Royo <esroyo@gmail.com>
2015-02-14 14:49:16 +0100

    Optimize css and js loading

--- custom/views/cubic/head.tpl.php
+++ custom/views/cubic/head.tpl.php
@@ -1,7 +1,7 @@
-    <link rel="stylesheet" href="custom/components/foundation/css/foundation.css" />
-    <link rel="stylesheet" href="custom/style/cubic.css" />
-    <link rel="stylesheet" href="custom/style/fontello.css">
-    <link rel="stylesheet" href="custom/style/animation.css"><!--[if IE 7]><link rel="stylesheet" href="custom/style/fontello-ie7.css"><![endif]-->
-    <link rel="alternate" type="application/atom+xml" title="ATOM" href="atom.php" />
-    <script src="custom/components/foundation/js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="custom/style/cubic.min.css" />
+    <!--[if IE 7]><link rel="stylesheet" href="custom/style/fontello/fontello-ie7.css"><![endif]-->
+    <link rel="alternate" type="application/atom+xml" title="ATOM" href="?type=atom10" />
+    <script src="custom/js/vendor/modernizr.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" defer></script>
+    <script src="custom/js/cubic.min.js" defer></script>
 
